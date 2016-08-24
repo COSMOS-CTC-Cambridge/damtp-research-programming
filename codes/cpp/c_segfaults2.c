fdc38a31155387003b8c9e00d644d059aee119c1
Juha Jaykka <juhaj@iki.fi>
2016-08-24 04:18:52 +0100

    Released at 2016-08-24T04:18:52+0100

--- codes/cpp/c_segfaults2.c
+++ codes/cpp/c_segfaults2.c
@@ -3,7 +3,6 @@
 double * invert_data(int sz) {
   double dest[20] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
   double * data = (double *) calloc(20,sizeof(double));
-  // [data] [sz] [*dest0 [*dest+1] ... [*dest+19]
   for (int ii=0; ii<=sz; ++ii) {
     dest[10+ii] = data[ii]+1;
   }
