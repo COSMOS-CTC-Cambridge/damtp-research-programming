ae49825d978204ea49145ce83a01564b1a75affa
Juha Jaykka <juhaj@iki.fi>
2017-10-20 12:45:46 +0100

    Released at 2017-10-20T12:45:46+01:00

---
+++ codes/python/mpi_hello_world_worst.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import mpi4py
+from mpi4py import MPI
+size=MPI.COMM_WORLD.Get_size()
+print("Hello, World. I am rank "+
+      "{rank: 0{len}d} of your MPI communicator of {size} ranks".format(
+      rank=MPI.COMM_WORLD.Get_rank(),
+      len=len(str(size)),
+      size=size))
